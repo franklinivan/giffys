cb116261dcd3445d7874ec1e7faad151c48b18fc
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-21 20:52:20 -0500

    add style

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,8 +1,10 @@
+import './Gif.css'
+
 export const Gif = ({ title, url }) => {
   return (
-    <>
+    <div className='Gif'>
       <h4>{title}</h4>
-      <img src={url} />
-    </>
+      <img src={url} alt={title} />
+    </div>
   )
 }
