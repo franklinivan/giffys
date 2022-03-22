d729d8737f04b0c283f11c35fae9892a9400e33d
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-21 20:37:55 -0500

    feat: search form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import { useState } from 'react'
 import { ListOfGifs } from './components/ListOfGifs'
+import './App.css'
 
 function App () {
+  const [keyword, setKeyword] = useState('morty')
+  const [value, setValue] = useState('')
+
+  const handleKeyword = e => {
+    console.log(value)
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setKeyword(value)
+  }
+
   return (
     <div className='App'>
       <section className='App-content'>
-        <ListOfGifs keyword='rick' />
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={handleKeyword} value={value} />
+          <button>search gif</button>
+        </form>
+        <ListOfGifs keyword={keyword} />
       </section>
     </div>
   )
