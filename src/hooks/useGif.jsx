34017f20625dc0abab8d4b9e1eae6e1e48658937
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-23 10:47:51 -0500

    feat: custom hook

---
+++ src/hooks/useGif.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import getGifs from '../services/getGifs'
+
+export const useGif = ({ keyword }) => {
+  const [gifs, setGifs] = useState([])
+
+  useEffect(() => {
+    getGifs({ keyword }).then(gifs => setGifs(gifs))
+  }, [keyword])
+
+  const getOneGif = id => gifs.find(gif => gif.id === id)
+
+  return {
+    gifs,
+    getOneGif
+  }
+}
