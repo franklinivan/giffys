f66547da77006cfcc227e3797a736073fd9b7820
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-23 10:47:30 -0500

    detail

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 /* global fetch */
 const { VITE_API_KEY } = import.meta.env
 
-export default function getGifs ({ keyword = 'gif' }) {
+export default function getGifs ({ keyword = 'gif' } = {}) {
   const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${VITE_API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
 
   return fetch(API_URL)
