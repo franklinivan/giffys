d75824f6aa2120b9b89e313d892c103fea90db5b
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-26 09:27:49 -0500

    style: change class name

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -5,7 +5,7 @@ export const Gif = ({ title, url, id }) => {
   const [path, setLocation] = useLocation() // eslint-disable-line
   return (
     <Link to={`${path}/${id}`}>
-      <div className='Gif'>
+      <div className='Gif-list-item'>
         <h4>{title}</h4>
         <img src={url} alt={title} />
       </div>
