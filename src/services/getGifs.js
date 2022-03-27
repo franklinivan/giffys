c13c17f69ae9f9dbeb0e4a1f9c0adac3ceaee3e7
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-27 18:01:19 -0500

    refactor: service get gifs

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,17 +1,20 @@
 /* global fetch */
 const { VITE_API_KEY } = import.meta.env
 
-export default function getGifs ({ keyword = 'gif' } = {}) {
-  const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${VITE_API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = response => {
+  const { data = [] } = response
+  if (Array.isArray(data)) {
+    return data.map(({ id, title, images }) => {
+      const { url } = images.downsized_medium
+      return { id, title, url }
+    })
+  }
+}
+
+export function getGifs ({ keyword = 'random', limit = '15' } = {}) {
+  const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${VITE_API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
 
   return fetch(API_URL)
     .then(res => res.json())
-    .then(({ data }) => {
-      const gifs = data.map(gif => {
-        const { id, title, images } = gif
-        const { url } = images.downsized_medium
-        return { id, title, url }
-      })
-      return gifs
-    })
+    .then(data => fromApiResponseToGifs(data))
 }
