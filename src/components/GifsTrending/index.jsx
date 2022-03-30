d442a9a2b9de4adcc81ac9389dc1a722d152a76f
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-30 11:20:36 -0500

    feat: lazy load to control request trends from api

--- src/components/GifsTrending/index.jsx
+++ src/components/GifsTrending/index.jsx
@@ -1,8 +1,9 @@
+import { useState, useEffect } from 'react'
 import { useTrends } from '@/hooks/useTrends'
 import { Link } from 'wouter'
 import './style.css'
 
-export const GifsTrending = () => {
+const GifsTrending = () => {
   const { trends } = useTrends()
   return (
     <div className='Gif-trend'>
@@ -12,6 +13,29 @@ export const GifsTrending = () => {
           <span>{title}</span>
         </Link>
       )}
+    </div>
+  )
+}
+
+export const LazyTrending = () => {
+  const [show, setShow] = useState(false) // eslint-disable-line
+
+  useEffect(() => {
+    const onChange = entries => {
+      const el = entries[0]
+      if (el.isIntersecting) setShow(true)
+    }
+
+    const observer = new IntersectionObserver(onChange, { // eslint-disable-line
+      rootMargin: '100px'
+    })
+
+    observer.observe(document.getElementById('Lazy-trending'))
+  })
+
+  return (
+    <div id='Lazy-trending'>
+      {show ? <GifsTrending /> : null}
     </div>
   )
 }
