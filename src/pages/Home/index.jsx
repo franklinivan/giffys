d1e7bf3f319f419246a6a7ac6e97cf605d7f3c3a
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-03-30 10:48:45 -0500

    add: styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,14 +2,20 @@ import { GifsForm } from '@/components/GifsForm'
 import { GifsList } from '@/components/GifsList'
 import { GifsTrending } from '@/components/GifsTrending'
 import { useGif } from '@/hooks/useGif'
+import './style.css'
 
 export default function Home () {
   const { gifs } = useGif()
+  const firstTime = !window.localStorage.getItem('lastSearch-keyword')
+
   return (
-    <>
+    <div className='home'>
       <GifsForm />
+      <p className='home-title'>
+        {firstTime ? null : 'Last search'}
+      </p>
       <GifsList gifs={gifs} />
       <GifsTrending />
-    </>
+    </div>
   )
 }
