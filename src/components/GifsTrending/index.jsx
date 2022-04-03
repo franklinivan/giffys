797f1ba010414bfdb07f475d73668a451603855d
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-04-03 18:07:42 -0500

    fix: useRef

--- src/components/GifsTrending/index.jsx
+++ src/components/GifsTrending/index.jsx
@@ -1,11 +1,12 @@
 import { useNearScreen } from '@/hooks/useNearScreen'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useRef } from 'react'
 import './style.css'
 
 const GifsTrending = lazy(() => import('@/components/GifsTrending/GifsTrending'))
 
 export const LazyTrending = () => {
-  const { isNearScreen, elementRef } = useNearScreen({ distance: '200px' })
+  const elementRef = useRef()
+  const { isNearScreen } = useNearScreen({ distance: '200px', elementRef })
 
   return (
     <div ref={elementRef}>
