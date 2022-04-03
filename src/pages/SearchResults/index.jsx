138496ff40f0185106ca698a13d85fd10879ab7a
Franklin Alvarado <franklinalvarado0921@gmail.com>
2022-04-03 14:02:19 -0500

    add: classname

--- src/pages/SearchResults/index.jsx
+++ src/pages/SearchResults/index.jsx
@@ -1,15 +1,16 @@
 import { GifsForm } from '@/components/GifsForm'
 import { GifsList } from '@/components/GifsList'
 import { useGif } from '@/hooks/useGif'
+import './style.css'
 
 export default function SearchResults ({ params }) {
   const { keyword } = params
   const { gifs } = useGif({ keyword })
 
   return (
-    <>
+    <div className='Search-results'>
       <GifsForm />
       <GifsList gifs={gifs} keyword={keyword} />
-    </>
+    </div>
   )
 }
